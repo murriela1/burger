39fa0bf688b3835652f8f427a5dcd6fecaa033c7
Andrew Murriel <murriela1@gmail.com>
2016-08-14 23:58:17 -0400

    UPDATED

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,23 +1,6 @@
-INSERT INTO burgers (burgerName) VALUES ("cheeseburger");
-INSERT INTO burgers (burgerName) VALUES ("bacon cheeseburger");
-INSERT INTO burgers (burgerName) VALUES	("turkey burger");
-INSERT INTO burgers	(burgerName) VALUES ("pizza burger");
-INSERT INTO	burgers (burgerName) VALUES ("cheeseburger deluxe");
-INSERT INTO	burgers (burgerName) VALUES ("cowboy burger");
-INSERT INTO burgers (burgerName) VALUES ("angus burger");
-INSERT INTO	burgers (burgerName) VALUES ("veggie burger");
-INSERT INTO burgers (burgerName) VALUES ("buffalo burger");
-INSERT INTO burgers (burgerName) VALUES ("bbq burger");
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT into burgers (burger_name,devoured) VALUES ('cheeseburger',FALSE);
+INSERT into burgers (burger_name,devoured) VALUES ('Turkey Burger',FALSE);
+INSERT into burgers (burger_name,devoured) VALUES ('Pizza Burger',FALSE);
+INSERT into burgers (burger_name,devoured) VALUES ('Angus Burger',FALSE);
+INSERT into burgers (burger_name,devoured) VALUES ('BBQ Burger',TRUE);
 
