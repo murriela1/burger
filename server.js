c54f27dfce3c5820465b628f6b2291ffc8327d79
Andrew Murriel <murriela1@gmail.com>
2016-08-15 00:03:10 -0400

    UPDATED

--- server.js
+++ server.js
@@ -1,38 +1,27 @@
 var express = require('express');
-var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
+var importRouter = require('./controllers/burgers_controller.js')['exportRouter']
 
 var app = express();
+var PORT = 3000;
 
-app.use(express.static(process.cwd() + '/public'));
-
-app.use(bodyParser.urlencoded({
-	extended: false
-
-}));
-
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(methodOverride('_method'));
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
-}));
-
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var routes = require('./controllers/burgers_controller.js');
-app.use('/', routes);
+app.use('/static', express.static('public/assets'));
+app.use(express.static('models'));
 
-var PORT = 3000;
-app.listen(PORT);
+app.use('/', importRouter);
+app.use('/create', importRouter);
+app.use('/update', importRouter);
 
-
-
-
-
-
-
-
-
-
-
+app.listen(PORT, function(){
+  console.log("Listening on port", PORT);
+});
